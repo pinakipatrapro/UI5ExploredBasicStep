e412b04bde4faf53a128a77d9a19994bf600fd03
Pinaki Patra <pinaki.patra.pro@gmail.com>
2018-06-05 10:45:51 +0530

    Change execution structure

--- src/test/java/com/org/UI5ExploredBasicStep/NavigateTest.java
+++ src/test/java/com/org/UI5ExploredBasicStep/NavigateTest.java
@@ -8,20 +8,24 @@ import com.org.UI5ExploredBasicStep.util.LoadDrivers;
 import com.org.UI5ExploredBasicStep.util.ElementHelper;
 
 public class NavigateTest {
+	static LoadDrivers drivers = new LoadDrivers();
+	private static final WebDriver wd = drivers.openUrl("https://sapui5.hana.ondemand.com");;
 
 	public ElementHelper search(String searchString,WebDriver wd) {
-		//Load Element Helper
+//		Load Element Helper
 		ElementHelper eh = new ElementHelper();
+		
 		eh.getElementById("sdk---app--searchControl-openingBtn-img", wd).click();
+		eh.getElementById("sdk---app--searchControl-searchField-I", wd).clear();
 		eh.getElementById("sdk---app--searchControl-searchField-I", wd).sendKeys(searchString);
 		eh.getElementById("sdk---app--searchControl-searchField-search", wd).click();
+		eh.getElementById("sdk---app--searchControl-closingBtn-inner", wd).click();
 		return eh;
 	}
 	@Test
 	public void navigateToFragmentSearch() {
 		//Load Drivers
-		LoadDrivers drivers = new LoadDrivers();
-		WebDriver wd = drivers.openUrl("https://sapui5.hana.ondemand.com");
+
 		ElementHelper eh = this.search("Fragment", wd);
 
 		String Path = "//*[@id='sdk---searchPage--allList-listUl']/descendant::a[contains(.,'Creating Custom Filter')]";
@@ -32,14 +36,13 @@ public class NavigateTest {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-		wd.close();
+		wd.navigate().back();
+		wd.navigate().back();
+		//		wd.close();
 	}
 	@Test
 	public void navigateToMVCSearch() {
 		//Load Drivers
-		LoadDrivers drivers = new LoadDrivers();
-		WebDriver wd = drivers.openUrl("https://sapui5.hana.ondemand.com");
 		ElementHelper eh = this.search("mvc", wd);
 
 		String Path = "//*[@id='sdk---searchPage--allList-listUl']/descendant::a[contains(.,'Application Cache Buster')]";
@@ -50,7 +53,8 @@ public class NavigateTest {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+		wd.navigate().back();
+		wd.navigate().back();
 		wd.close();
 	}
 }
