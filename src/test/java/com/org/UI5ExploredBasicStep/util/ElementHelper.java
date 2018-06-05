95c32fb093675b1d4a859cabb3b0007050f8f615
Pinaki Patra <32954893+pinakipatrapro@users.noreply.github.com>
2018-06-05 08:54:39 +0530

    Update ElementHelper.java

--- src/test/java/com/org/UI5ExploredBasicStep/util/ElementHelper.java
+++ src/test/java/com/org/UI5ExploredBasicStep/util/ElementHelper.java
@@ -11,14 +11,14 @@ public class ElementHelper {
 		System.out.println("Finding element with ID : " + id);
 		WebDriverWait wait = new WebDriverWait(wd, 15);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(id)));
-		System.out.println("Success. " + id + " found");
+		System.out.println("Success. " + id + " found !!");
 		return wd.findElement(By.id(id));
 	}
 	public WebElement getElementByXpath(String xpath,WebDriver wd) {
 		System.out.println("Finding element with XPath : " + xpath);
 		WebDriverWait wait = new WebDriverWait(wd, 15);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
-		System.out.println("Success. " + xpath + " found");
+		System.out.println("Success. " + xpath + " found !!");
 		return wd.findElement(By.xpath(xpath));
 	}
 }
